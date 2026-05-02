ab452c0809baa5dd204221da857fa6ded517b9fd
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-05-02 17:07:29 +0000

    Added the observable function

--- practiceObservable.ts
+++ practiceObservable.ts
@@ -1,1 +1,36 @@
-export interface Observer<T>{}
+import type { Subscription } from "./observable.js";
+
+export interface Observer<T>{
+    next(value:T):void;
+    error(err:any):void;
+    cpmplete():void;
+}
+export type CleanupFunction=()=>void;
+
+export type Executor<T>=(observer:Observer<T>)=>CleanupFunction|void;
+
+export type Subsrciption={
+    unsubsribe():void;
+
+}
+
+export class Observable<T>{
+    private executor:Executor<T>;
+    constructor(executor:Executor<T>){
+        this.executor=executor;
+
+    }
+
+    subscribe(observer:Observer<T>):Subscription{
+        const cleanup=this.executor(observer);
+    }
+    return{
+        unsubscribe(){
+            if(cleanup){
+                cleanup();
+            }
+        }
+    }
+}
+
+
